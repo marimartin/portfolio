7890baf0a2b0b1507a9852f08367d07b9a614de3
Mari <marinarosemartin@gmail.com>
2020-08-27 16:35:49 -0500

    updated portfolio

--- README.md
+++ README.md
@@ -4,17 +4,6 @@ Published site:
 https://marimartin.github.io/portfolio/
 
 
-![](assets/screenshots/about.jpg)
+<img src="assets/gif/portfolio.gif" height="500">
 
-![](assets/screenshots/portfolio.jpg)
-
-![](assets/screenshots/contact.jpg)
-
-<img src="assets/screenshots/mobile-navbar.jpg" height="500">
-
-<img src="assets/screenshots/mobile-about.jpg" height="500">
-
-<img src="assets/screenshots/mobile-portfolio.jpg" height="500">
-
-<img src="assets/screenshots/mobile-contact.jpg" height="500">
 
---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+
+
+$(".fa-linkedin-square").click(function () {
+    window.location.replace("https://www.linkedin.com/in/marina-martin-b40859a2/");
+});
+
+$(".fa-github").click(function () {
+    window.location.replace("https://github.com/marimartin");
+});
+
+$(".fa-envelope").click(function () {
+    window.location.replace("pages/contact.html");
+});
+
+$("#book-market").click(function () {
+    window.location.replace("https://github.com/marimartin/Book-Market");
+});
+
+$("#weather-dashboard").click(function () {
+    window.location.replace("https://github.com/marimartin/weather-dashboard");
+});
+
+$("#day-planner").click(function () {
+    window.location.replace("https://github.com/marimartin/day-planner");
+});
+
